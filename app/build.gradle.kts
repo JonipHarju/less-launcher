49f2549c8e2a3ca1231727cdb08833c570e68472
jonipHarju <jonipharju@gmail.com>
2026-08-20 14:50:58 +0300

    Raise Less to version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.jonipharju.less"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
